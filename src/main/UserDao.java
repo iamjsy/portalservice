8b3bf07bc0530006c30d1bbaf34f8f034fae3d45
416-23 <iamjsy>
2018-04-06 16:05:23 +0900

    이제야..

---
+++ src/main/UserDao.java
@@ -0,0 +1,48 @@
+import java.sql.*;
+
+public class UserDao {
+    private final ConnectionMaker connectionMaker;
+
+    public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+
+    public User get(int id) throws ClassNotFoundException, SQLException {
+        Connection connection = connectionMaker.getConnection();
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("select * from userinfo where id = ?");
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        return user;
+    }
+
+    public Integer insert(User user) throws SQLException, ClassNotFoundException {
+        Connection connection = connectionMaker.getConnection();
+        PreparedStatement preparedStatement =
+                connection.prepareStatement(
+                        "insert into userinfo(name, password) values (?, ?)");
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement = connection.prepareStatement("select last_insert_id()");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        Integer id = resultSet.getInt(1);
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return id;
+    }
+}
